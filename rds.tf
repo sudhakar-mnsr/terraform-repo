af868c3bf906f2ead5ef58996ca79f089fd677c2
MNSR Sudhakar <sudhakar.mnsr@gmail.com>
2022-12-02 13:21:25 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "myec2" {
     ami = "ami-094125af156557ca2"
     instance_type = "t2.micro"
+    tags = {
+      Name = "HelloWorld"
+    }
 }
 /* resource "aws_db_instance" "default" {
   allocated_storage    = 5
